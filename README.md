268b13f2509b25d3742c0559b587816b71aaad09
fralps <francois@kinoba.fr>
2022-04-08 17:36:28 +0200

    chore(cemantix): initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cemantix-solver
---
+++ script.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from requests.structures import CaseInsensitiveDict
+
+# Opening JSON file
+file = open('index.json')
+ 
+# Returns JSON object as a dictionary
+words = json.load(file)
+
+# Cemantix API URL
+url = 'https://cemantix.herokuapp.com/score'
+
+# Headers definitions
+headers = CaseInsensitiveDict()
+headers['Content-Type'] = 'application/x-www-form-urlencoded'
+ 
+# Iterating through the json list
+for word in words:
+  data = f'word={word}'
+
+  resp = requests.post(url, headers=headers, data=data)
+    
+  score = resp.json()
+  
+  if 'score' in score:
+    if score['score'] > 0.5:
+      print(f"{word} --> {score['score']}")
+    
+    if score['score'] == 1:
+      print(f'Result: {word}')
+      break
+ 
+# Closing file
+file.close()
