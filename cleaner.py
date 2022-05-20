297085cdef67ae2d3d7a76d6335c4c0921ba66f5
fralps <francois@kinoba.fr>
2022-05-20 14:52:56 +0200

    fix: re-change words list

---
+++ cleaner.py
@@ -0,0 +1,45 @@
+# Modules definitions
+import requests
+from requests.structures import CaseInsensitiveDict
+
+# Prepare words list
+words = []
+
+# Parse the txt file,
+# and store the words in a list
+with open('dictionnaries/english3.txt') as f:
+  line = f.readline()
+  while line:
+    line = f.readline()
+    words.append(line.rstrip('\n'))
+
+# Cemantle API URL
+url = 'https://cemantle.herokuapp.com/score'
+
+# Headers definitions
+headers = CaseInsensitiveDict()
+headers['Content-Type'] = 'application/x-www-form-urlencoded'
+headers['Host'] = 'cemantle.herokuapp.com'
+headers['Origin'] = 'https://cemantle.herokuapp.com'
+headers['referrer'] = 'https://cemantle.herokuapp.com/'
+headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+
+# Iterating through the txt list
+def main():
+  # for word in words[words.index('pompages'):]:
+  for word in words:
+    data = f'word={word}'.encode('utf-8')
+    resp = requests.post(url, headers=headers, data=data)
+    score = resp.json()
+
+    if 'score' in score:
+      print('SCORE:', score['score'])
+      clean(word)
+    else:
+      print(f"{word} - {score}")
+
+def clean(word):
+  with open('dictionnaries/cleaned.txt', 'a') as f:
+    f.write(f"{word}\n")
+
+main()
