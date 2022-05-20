404a1a147e15adb9e1b565fa00a15e6d7fd7a0a1
fralps <francois@kinoba.fr>
2022-05-20 18:37:13 +0200

    feat: wip en cleaner list

--- cemantle_solver.py
+++ cemantle_solver.py
@@ -49,7 +49,7 @@ def main():
   threading.Thread(target=solve, args=[True, True]).start()
   
   # Random threads
-  for i in range(50):
+  for i in range(40):
     print(f"Starting thread n°{i + 1}")
     threading.Thread(target=solve, args=[True, False]).start()
 
--- cleaner.py
+++ cleaner.py
@@ -26,8 +26,7 @@ headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 
 # Iterating through the txt list
 def main():
-  # for word in words[words.index('pompages'):]:
-  for word in words:
+  for word in words[words.index('austerlitz'):]:
     data = f'word={word}'.encode('utf-8')
     resp = requests.post(url, headers=headers, data=data)
     score = resp.json()
